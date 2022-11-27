c9261e6b3c71b52df29ca1bd2855cdcfe01176bf
AlekseyBaboshin <BaboshinAP@gmail.com>
2022-11-27 23:49:20 +0300

    Created the ptogram in folder by Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.Write("Введите день недели (в виде числа от 1 до 7): ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 0 & day <= 7)
+{
+    if (day == 6 | day == 7)
+    {
+        Console.WriteLine($"{day}-й день недели это выходной день!");
+    }
+
+    else
+    {
+        Console.WriteLine($"{day}-й день недели не является выходным днем!");
+    }
+}
+
+else
+{
+    Console.WriteLine($"Введеное число {day} не может являеться днем недели!");
+}
