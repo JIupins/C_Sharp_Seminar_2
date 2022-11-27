da2b67bb906daa540ba0cb5cf6f93aec1af82188
AlekseyBaboshin <BaboshinAP@gmail.com>
2022-11-27 23:37:28 +0300

    Created the ptogram in folder by Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number / 100) >= 1)
+{
+    int buffer = number;
+    
+    while ((buffer / 100) >= 10)
+    {
+        buffer /= 10;
+    }
+
+    int result = buffer % 10;
+    Console.WriteLine($"Третья цифра в числе {number} это: {result}.");
+}
+else
+{
+    Console.WriteLine($"В числе {number} меньше трех символов.");
+}
