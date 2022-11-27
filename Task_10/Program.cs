88fa27eab8bad8f858bc7211d7fe171533455c86
AlekseyBaboshin <BaboshinAP@gmail.com>
2022-11-27 23:35:39 +0300

    Created the ptogram in folder by Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number / 100) >= 1 & (number / 100) < 10)
+{
+    int result = (number / 10) % 10;
+    Console.WriteLine ($"Вторая цифра в трехзначном числе {number} это: {result}.");
+}
+else
+{
+    Console.WriteLine ($"Введеное число {number} не является трехзначным.");
+}
